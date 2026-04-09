88d9ffea94e1a09b1d591b6994ed0e69b42cf1ea
alieray123 <alieray.ozdemir@std.yeditepe.edu.tr>
2026-04-09 12:52:06 +0300

    fix: unset STREAMLIT_SERVER_PORT before running streamlit

--- start.sh
+++ start.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+# Unset problematic env var that Railway sets
+unset STREAMLIT_SERVER_PORT
+
+# Run streamlit with explicit port
 streamlit run app.py \
   --server.port=${PORT:-8501} \
   --server.address=0.0.0.0 \
